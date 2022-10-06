59189bff61d9295e9da5476afb93a8caa47cd900
pushkar45756 <pushkar.srivastava@infostride.com>
2022-10-07 01:26:59 +0530

    + added DIP + updated SRP

--- src/solid/DependencyInversionP.java
+++ src/solid/DependencyInversionP.java
@@ -3,4 +3,88 @@ package solid;
 
 // It is based on the Open/Closed Principle and the Liskov Substitution Principle.
 public class DependencyInversionP {
+
 }
+
+
+class Book {
+    public void seeReviews() {
+        System.out.println("Seeing Reviews");
+    }
+
+    public void getSample() {
+        System.out.println("Getting Sample");
+    }
+}
+
+class Shelf {
+    Book book;
+
+
+    public void addBook() {
+        System.out.println("Book Added on Shelf");
+    }
+
+    public void customizeShelf() {
+        System.out.println("Shelf Rearranged");
+    }
+}
+
+class DVD {
+    public void seeReviews() {
+        System.out.println("Seeing Reviews");
+    }
+
+    public void getSample() {
+        System.out.println("Playing Sample");
+    }
+}
+
+interface Product {
+
+    public void seeReview();
+
+    public void getSample();
+}
+
+        class BookV2 implements Product {
+
+            @Override
+            public void seeReview() {
+                System.out.println("Seeing Book Reviews");
+            }
+
+            @Override
+            public void getSample() {
+                System.out.println("Getting Book Sample");
+            }
+        }
+
+        class DVDV2 implements Product {
+
+            @Override
+            public void seeReview() {
+                System.out.println("Seeing Movie Reviews");
+            }
+
+            @Override
+            public void getSample() {
+                System.out.println("Getting DVD Sample");
+            }
+        }
+
+        class ShelfV2{
+            Product product;
+
+            public void addProduct() {
+                System.out.println("Product Added on Shelf");
+            }
+
+            public void customizeShelf() {
+                System.out.println("Shelf Rearranged");
+            }
+        }
+
+
+
+
--- src/solid/SingleResponsibilityP.java
+++ src/solid/SingleResponsibilityP.java
@@ -7,8 +7,7 @@ public class SingleResponsibilityP {
 }
 
 
-
-class  User{
+class User {
     String email;
     String name;
 
@@ -29,12 +28,46 @@ class  User{
     }
 
 
-
     /// This method should not be there as it is directly not related to User
     // This is violating the concept of Single Responsibility Principle
-    public void registerUser(){
+    public void registerUser(User user) {
         // logic to register User
     }
 
+}
+
+
+class User2 {
+    String email;
+    String name;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }
+
+
+class UserRepository {
+
+    User2 user;
+
+    public void registerUser(User2 user) {
+        this.user = user;
+        // logic to save user
+    }
+}
+
+
